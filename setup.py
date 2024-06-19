98c99de33a1c45d40111313d936d5f3d1424732c
rohit <rkharat6388@gmail.com>
2024-06-19 22:46:27 +0530

    cookie cutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Recommends top 3 food items.',
+    author='Rohit Kharat',
+    license='',
+)
